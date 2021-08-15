01c27fae623b9554ef5ca643afb71e882fdbe353
voanhkhoa19102001 <80662151+voanhkhoa19102001@users.noreply.github.com>
2021-08-15 23:14:49 +0700

    abc

--- Connection.php
+++ Connection.php
@@ -1,38 +1,14 @@
-<?php
-    function getData($rs){
-        $data = array();
-
-        if (mysqli_num_rows($rs) <= 0) {
-            echo "Data not found";
-        } else {
-
-            while ($row = mysqli_fetch_assoc($rs)) {
-                $data[] = $row;
-            }
+<?php 
+        $servername = "localhost";
+        $database = "databasename";
+        $username = "username";
+        $password = "password";
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
         }
-        return $data;
-    }
-
-    function getResultSQL($qry)
-    { 
-        $conn = getConnection();
-        return mysqli_query($conn, $qry);
-    }
-
-    function getConnection(){
-        $dbhost = "localhost";
-        $dbuser = "root";
-        $dbpass = "123456";
-        $db = "cuahangtrangsuc";
-        $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
-        return $conn;
-    }
-
-    function sqlUpdate($qry){
-        $conn = getConnection();
-        if(mysqli_query($conn,$qry)){
-            return true;
-        }
-        return false;
-    }
+        echo "Connected successfully";
+        mysqli_close($conn);
 ?>
